89509756cdda9a2bc14d1b7c239ea562b2cb4c32
ivanpopov1109 <ivanpopov1109@gmail.com>
2021-12-15 14:24:21 +0300

    добавил  маршрутов для приложения News в контроллер маршрутов mysite

---
+++ news/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path
+from .views import *
+# Точка указывает на текущую директорию из которой из модуля views мы импортируем все функции
+urlpatterns = [
+    path('', index),
+    path('test/', test)
+
+]
