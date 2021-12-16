8b06294e214bd01c54137186dd0eb25dc3018191
ivanpopov1109 <ivanpopov1109@gmail.com>
2021-12-17 00:38:10 +0300

    Настроил админку, изменил модели news, что бы можо было делать записи без фото

--- news/admin.py
+++ news/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import News
+class NewsAdmin(admin.ModelAdmin): # создадим класс редактора, в котором можено настроить представление модели в админке
+    # он должен быть подклассом редактора admin.ModelAdmin
+    list_display = ('id', 'title', 'created_at', 'updated_at', 'is_published')
+    list_display_links = ('id', 'title') # чтобы в админке ссылкой был не только номер записи но еще и title
+    search_fields = ('title', 'content')# поиск в админке можео осуществлять по полям title и  content
+
+admin.site.register(News, NewsAdmin) #здесь важен порядок разрешения моделей, сначала регистрируеся основная модель
--- news/models.py
+++ news/models.py
@@ -1,14 +1,18 @@
 from django.db import models
 
 class News(models.Model):
-    title = models.CharField(max_length=150)
-    content = models.TextField(blank=True)
-    created_at = models.DateTimeField(auto_now_add=True)
-    updated_at = models.DateTimeField(auto_now=True)
-    photo = models.ImageField(upload_to='photos/%Y/%m/%d/')
-    is_published = models.BooleanField(default=True)
+    title = models.CharField(max_length=150, verbose_name='Наиенование')
+    content = models.TextField(blank=True, verbose_name='Контент')
+    created_at = models.DateTimeField(auto_now_add=True, verbose_name='Дата публикации')
+    updated_at = models.DateTimeField(auto_now=True, verbose_name='Обновлено')
+    photo = models.ImageField(upload_to='photos/%Y/%m/%d/', verbose_name='Фото', blank=True) # blank=True Делает поле не обязательным
+    is_published = models.BooleanField(default=True, verbose_name='Опобликовано?')
 
     def __str__(self): # что бы в выводе News.object.all() у нас было строковое представление названи объекта а не название приложения и его идентификатор
         return self.title
 
-# Create your models here.
+    class Meta: #
+        verbose_name = 'Новость' # наименование модели в единственном числе
+        verbose_name_plural = 'Новости'
+        ordering = ['-created_at']
+
