a2a3e8b858d2c03b34f725ecd84c453d382949b7
ivanpopov1109 <ivanpopov1109@gmail.com>
2021-12-21 17:28:19 +0300

    Добавил имена маршрутов categegoty

--- news/models.py
+++ news/models.py
@@ -1,7 +1,7 @@
 from django.db import models
 
 class News(models.Model):
-    title = models.CharField(max_length=150, verbose_name='Наиенование')
+    title = models.CharField(max_length=150, verbose_name='Наименование')
     content = models.TextField(blank=True, verbose_name='Контент')
     created_at = models.DateTimeField(auto_now_add=True, verbose_name='Дата публикации')
     updated_at = models.DateTimeField(auto_now=True, verbose_name='Обновлено')
--- news/urls.py
+++ news/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from .views import *
 # Точка указывает на текущую директорию из которой из модуля views мы импортируем все функции
 urlpatterns = [
-    path('', index),
-    path('category/<int:category_id>/', get_category)
+    path('', index, name='home'),
+    path('category/<int:category_id>/', get_category, name='category')
 
 ]
